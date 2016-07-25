c477d0779c59a4e0b1770514bedc4cfb40b9023c
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-07-25 15:21:34 +0200

    update app

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.get('/', (req, res) => {
-  console.error('express connection');
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+
+app.use(express.static(path.join(__dirname, 'html')));
+
 io.on('connection', s => {
   console.error('socket.io connection');
   for (var t = 0; t < 3; t++)
     setTimeout(() => s.emit('message', 'message from server'), 1000*t);
 });
+
 http.listen(3002, () => console.error('listening on http://localhost:3002/'));
+
 console.error('socket.io example');
 
